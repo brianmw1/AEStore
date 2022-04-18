c0f82c473dbaa342cdafb2995d40d1215304326f
bryanbarcos <bryanbarcos@hotmail.ca>
2022-04-18 15:30:11 -0400

    display order status

--- public/controllers/checkout.js
+++ public/controllers/checkout.js
@@ -16,9 +16,7 @@ app.controller('Checkout', function($scope, $location, $http, $rootScope) {
 			console.log(userOrder);
 			$http.post('http://localhost:5000/cart/checkout', userOrder).
 				then(function(response) {
-					console.log(response);
 					$scope.status = response.data.status;
-					console.log($scope.status);
 				})
 		}
 		
